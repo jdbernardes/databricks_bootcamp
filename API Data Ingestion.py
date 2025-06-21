a5e84f0873ffbda3a373a5677e6c1ba1d4436a88
jdbernardes <jdbernardes>
2025-06-21 19:30:53 +0000

    Completing BTC API Ingestion script

--- API Data Ingestion.py
+++ API Data Ingestion.py
@@ -4,6 +4,7 @@ from pyspark.sql.types import StructType, StructField, StringType, FloatType, Ti
 from datetime import datetime
 
 url = dbutils.secrets.get(scope="databricks_bootcamp", key="API_URL")
+table_name = "bronze.bitcoin_price"
 
 def fetch_bitcoin_price():
     """Function created to get BTC price from API and return a Spark DataFrame with the following columns:
@@ -15,7 +16,7 @@ def fetch_bitcoin_price():
         data = response.json()['data']
         # Adding timestamp to the data
         return{
-            "amount": data['amount'],
+            "amount": float(data['amount']),
             "base": data['base'],
             "currency": data['currency'],
             "datetime": datetime.now()
@@ -24,12 +25,31 @@ def fetch_bitcoin_price():
         print(f"Error fetching data: {e}")
         return None
 
+def save_to_table(data):
+    """Function created to save the data to a table in the database."""
+    schema = StructType([
+        StructField("amount", FloatType(), True),
+        StructField("base", StringType(), True),
+        StructField("currency", StringType(), True),
+        StructField("datetime", TimestampType(), True)
+    ])
+    data = spark.createDataFrame([data], schema = schema)
+    data.write.format("delta").mode("append").saveAsTable(table_name)
+    print("Data saved to table")
+
+
 if __name__ == "__main__":
     # Get API Data
-    print("Getting API Data...")
+    print("Getting API Data and saving in Delta table")
     bitcoin_price = fetch_bitcoin_price()
-    print("Data fetched successfully!")
+    save_to_table(bitcoin_price)
+    print("Data saved successfuly!")
     print(bitcoin_price)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from bronze.bitcoin_price
 
 # COMMAND ----------
 
